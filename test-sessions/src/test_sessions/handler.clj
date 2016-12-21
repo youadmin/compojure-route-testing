2c9c57141580a6cb6d978694333886ac476c5434
derek <dlbuchner@gmail.com>
2016-12-21 09:27:45 -0500

    adding sessions example

---
+++ test-sessions/src/test_sessions/handler.clj
@@ -0,0 +1,51 @@
+(ns test-sessions.handler
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]])
+	(:require [ring.middleware.session :refer [wrap-session]]))
+
+(defn wrap-one
+	[handler]
+	(fn [request]
+		(do 
+			(print "IN WRAP ONE\n")
+			(handler request)
+		)
+	)
+)
+
+(def need-middle-routes
+	(->
+		(routes
+  		(GET "/two" 
+				[] 
+				"Hello two WRAP One"
+			)
+  		(GET "/one" 
+				[] 
+				"Hello one WRAP One"
+			))
+			(wrap-session)
+			(wrap-routes wrap-one)))
+
+(defroutes no-middle-routes
+  (GET "/no" [] "Hello no wrap")
+  (route/not-found "Not Found"))
+
+(def app-routes 
+	(routes
+		need-middle-routes
+		no-middle-routes))
+
+(def app app-routes)
+
+(comment (str
+	"from professional clojure...."
+	"(def body-routes (-> (route (GET etc) (wrap-routes mid-one))))"
+	"(defroutes other-routes (..)"
+
+	"and then combine them...."
+	"(def app-routes (routes body-routes other-routes))"
+	"finallly"
+	"(def app (-> app-routes wrap-500-catchall (wrap-defaults api-defaults)))"
+))
---
+++ test-sessions/test/test_sessions/handler_test.clj
@@ -0,0 +1,42 @@
+(ns test-sessions.handler-test
+  (:require [clojure.test :refer :all]
+						[midje.sweet :refer :all]
+            [ring.mock.request :as mock]
+            [test-sessions.handler :refer :all]))
+
+(facts "hello returns the correct status"
+	(fact "the wrapped call returns 200"
+		(let [
+        nada (print "CALL FOR one, wrapped\n")
+				resp 
+					(app (mock/request :get "/one"))
+				status
+					(:status resp) 
+			]
+			status
+		) => 200
+	)
+	(fact "the second wrapped call returns 200"
+		(let [
+        nada (print "CALL FOR two, wrapped\n")
+				resp 
+					(app (mock/request :get "/two"))
+				status
+					(:status resp) 
+			]
+			status
+		) => 200
+	)
+	(fact "the unwrapped call returns 200"
+		(let [
+        nada (print "CALL FOR no, unwrapped\n")
+				resp 
+					(app (mock/request :get "/no"))
+				status
+					(:status resp) 
+			]
+			status
+		) => 200
+	)
+)
+
